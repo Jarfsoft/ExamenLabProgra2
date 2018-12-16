1ef63cc0c1eca818ae9735e7b5f6a603520df882
Jarfsoft <42102133+Jarfsoft@users.noreply.github.com>
2018-12-15 18:57:23 -0600

    Update Jugadores.java

--- ExamenII_LabPrograII/src/examenii_labprograii/Jugadores.java
+++ ExamenII_LabPrograII/src/examenii_labprograii/Jugadores.java
@@ -12,12 +12,6 @@ import java.io.IOException;
  * @author Jarfsoft
  */
 public class Jugadores {
-    private int codigoEquipo;
-    private String nombre;
-    private int dorsal;
-    private String posicion;
-    private int edad;
-    private String nacionalidad;
     private RandomAccessFile jd;
     public Jugadores(){
         
@@ -33,6 +27,8 @@ public class Jugadores {
         jd.seek(jd.length());
         jd.writeInt(codigoEquipo);
         jd.writeUTF(nombre);
+        jd.writeInt(dorsal);
+        jd.writeUTF(posicion);
         jd.writeInt(edad);
         jd.writeUTF(nacionalidad);
     }
@@ -52,4 +48,118 @@ public class Jugadores {
             }
         }
     }
-}
+    public int cantidadJugadoresEquipo(int codigo) throws IOException{
+        int cantidad=0;
+        jd.seek(0);
+        while(jd.getFilePointer()!=jd.length()){
+            if(jd.readInt()==codigo)
+                cantidad++;
+            jd.readUTF();
+            jd.readInt();
+            jd.readUTF();
+            jd.readInt();
+            jd.readUTF();
+        }
+        return cantidad;
+        
+    }
+    public int cantidadJugadores()throws IOException{
+        jd.seek(0);
+        int cant=0;
+        while(jd.getFilePointer()!=jd.length()){
+            jd.readInt();
+            jd.readUTF();
+            jd.readInt();
+            jd.readUTF();
+            jd.readInt();
+            jd.readUTF();
+            cant++;
+        }
+        return cant;
+    }
+    public void imprimirJugador(int pos, boolean dorsal)throws IOException{
+        long posi=jd.getFilePointer();
+        int act=0;
+        jd.seek(0);
+        do{
+            act++;
+            jd.readInt();
+            if(pos==act){
+                if(dorsal)
+                    System.out.println(jd.readUTF()+"\t"+jd.readInt());
+                else{
+                    System.out.println(jd.readUTF());
+                    jd.readInt();
+                }
+                
+            }
+            jd.readUTF();
+            jd.readInt();
+            jd.readUTF();
+            
+        }while(act!=pos);
+        jd.seek(posi);
+    }
+    public void imprimirJugadores()throws IOException{
+        int[][] jugadores= new int[cantidadJugadores()][2];
+        jd.seek(0);
+        for(int x=0;x<jugadores.length;x++){
+            jd.readInt();
+            jd.readUTF();
+            jd.readInt();
+            jd.readUTF();
+            jugadores[x][0]=x+1;
+            jugadores[x][1]=jd.readInt();
+            jd.readUTF();
+        }
+        int temp=0;
+        int temp1=0;
+        for(int i=0;i<jugadores.length;i++){
+            for(int j=1;j<jugadores.length-i;j++){
+                if(jugadores[j-1][1]>jugadores[j][1]){
+                    temp=jugadores[j-1][0];
+                    temp1=jugadores[j-1][1];
+                    jugadores[j-1][0]=jugadores[j][0];
+                    jugadores[j-1][1]=jugadores[j][1];
+                    jugadores[j][0]=temp;
+                    jugadores[j][1]=temp1;
+                }
+            }
+        }
+        for(int x=0;x<jugadores.length;x++){
+            imprimirJugador(jugadores[x][0],false);
+        }
+    }
+    public void imprimirJugadoresEquipo(int code)throws IOException{
+        jd.seek(0);
+        int pos=0;
+        while(jd.getFilePointer()!=jd.length()){
+            pos++;
+            if(jd.readInt()==code)
+                imprimirJugador(pos,true);
+            jd.readUTF();
+            jd.readInt();
+            jd.readUTF();
+            jd.readInt();
+            jd.readUTF();
+        }
+    }
+    public void imprimirJugadoresPosicion(String posicion) throws IOException{
+        jd.seek(0);
+        long loc=0;
+        while(jd.getFilePointer()!=jd.length()){
+            loc=jd.getFilePointer();
+            jd.readInt();
+            jd.readUTF();
+            jd.readInt();
+            if(jd.readUTF().equals(posicion)){
+                jd.seek(loc);
+                jd.readInt();
+                System.out.println(jd.readUTF());
+                jd.readInt();
+                jd.readUTF();
+            }
+            jd.readInt();
+            jd.readUTF();
+        }
+    }
